e2cb3286e9d93b1e6b7659088c47473eff27a71c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-02-14 16:05:49 +0100

    Add login test cases

---
+++ Login/Login.Tests.ps1
@@ -0,0 +1,18 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Login" {
+    It "Logs in correctly" {
+        $true | Should Be $true
+    }
+    It "Shows Jacqueline in the list of contacts" {
+        $true | Should Be $true
+    }
+    It "Shows Jacqueline's contact details" {
+        $true | Should Be $true
+    }
+    It "Can update Jacqueline's contact details" {
+        $true | Should Be $true
+    }
+}
